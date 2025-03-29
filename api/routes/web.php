ebe1dc3a34bd096f26585083d8e663ad03f7c00b
Gabriel Santos <gabriel.code.org@gmail.com>
2025-03-29 09:02:36 -0300

    chore: default root path to json response instead of a view

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(["status" => "live", "version" => 1, "status_code" => HttpResponse::HTTP_OK], HttpResponse::HTTP_OK);
 });
