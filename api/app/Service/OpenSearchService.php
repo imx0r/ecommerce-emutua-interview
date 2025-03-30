c49e016cd81e05225c44fe4cb9bfe8ea186d8efa
Gabriel Santos <gabriel.code.org@gmail.com>
2025-03-30 12:40:11 -0300

    chore(service): create OpenSearch client

--- api/app/Service/OpenSearchService.php
+++ api/app/Service/OpenSearchService.php
@@ -2,7 +2,16 @@
 
 namespace App\Service;
 
+use OpenSearch\ClientBuilder;
+
 class OpenSearchService
 {
+    protected \OpenSearch\Client $client;
 
+    public function __construct()
+    {
+        $this->client = ClientBuilder::create()
+            ->setHosts([env('OPENSEARCH_HOSTS', 'http://opensearch:9200')])
+            ->build();
+    }
 }
