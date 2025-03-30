7c486437843eb21c71a731a549f95a931334d305
Gabriel Santos <gabriel.code.org@gmail.com>
2025-03-30 16:24:54 -0300

    chore(service): added `indexProduct` method to opensearch service

--- api/app/Service/OpenSearchService.php
+++ api/app/Service/OpenSearchService.php
@@ -14,4 +14,14 @@ class OpenSearchService
             ->setHosts([env('OPENSEARCH_HOSTS', 'http://opensearch:9200')])
             ->build();
     }
+
+    public function indexProduct(array $product): array|string|null
+    {
+        $params = [
+            'index' => 'products',
+            'id' => $product['id'],
+            'body' => $product
+        ];
+        return $this->client->index($params);
+    }
 }
