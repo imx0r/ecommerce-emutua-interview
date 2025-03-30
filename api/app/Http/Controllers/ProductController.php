8e7f15f9f5bc21a56502d71a75d2dc9408c0d34d
Gabriel Santos <gabriel.code.org@gmail.com>
2025-03-30 20:40:23 -0300

    refactor: use `OpenSearchService` to index or remove product when needed and general improvements

--- api/app/Http/Controllers/ProductController.php
+++ api/app/Http/Controllers/ProductController.php
@@ -27,7 +27,7 @@ class ProductController extends Controller
 
     public function index(Request $request): \Illuminate\Http\JsonResponse
     {
-        $products = Cache::remember('products', now()->addMinutes(30), function () {
+        $products = Cache::remember(config('cache.keys.product.products.key'), config('cache.keys.product.products.ttl'), function () {
             return $this->products->all();
         });
         return response()->json($products, HttpResponse::HTTP_OK);
@@ -36,22 +36,22 @@ class ProductController extends Controller
     public function show(Request $request, int $id): \Illuminate\Http\JsonResponse
     {
         $product = Cache::remember(Str::replace(':id', $id, config('cache.keys.product.product.key')), config('cache.keys.product.product.ttl'), function () use ($id) {
-            return $this->products->byId($id)->toArray();
+            return $this->products->byId($id);
         });
-        if ($product) {
-            $this->openSearchService->indexProduct($product->toArray());
-        }
         return response()->json($product, HttpResponse::HTTP_OK);
     }
 
     public function store(CreateOrUpdateProductRequest $request): \Illuminate\Http\JsonResponse
     {
-        if (!$request->user()->isAdministrator()) {
+        if (!$this->userIsAdministrator()) {
             return response()->json(["status" => EResponseStatus::ERROR, "message" => __("product.create.forbidden")], HttpResponse::HTTP_FORBIDDEN);
         }
 
         try {
             $product = $this->products->createProduct($request->validated());
+            if ($product) {
+                $this->openSearchService->indexProduct($product->toArray());
+            }
             return response()->json($product->toArray(), HttpResponse::HTTP_CREATED);
         } catch (ProductException $e) {
             return response()->json(["status" => EResponseStatus::FAILED, "message" => $e->getMessage()], HttpResponse::HTTP_BAD_REQUEST);
@@ -60,13 +60,15 @@ class ProductController extends Controller
 
     public function update(CreateOrUpdateProductRequest $request, int $id): \Illuminate\Http\JsonResponse
     {
-        if (!$request->user()->isAdministrator()) {
+        if (!$this->userIsAdministrator()) {
             return response()->json(["status" => EResponseStatus::ERROR, "message" => __("product.update.forbidden")], HttpResponse::HTTP_FORBIDDEN);
         }
 
         try {
-            $product = $this->products->byId($id);
-            $product = $this->products->updateProduct($product, $request->validated());
+            $product = $this->products->updateProduct($this->products->byId($id), $request->validated());
+            if ($product) {
+                $this->openSearchService->indexProduct($product->toArray());
+            }
             return response()->json($product->toArray(), HttpResponse::HTTP_OK);
         } catch (ProductException $e) {
             return response()->json(["status" => EResponseStatus::FAILED, "message" => $e->getMessage()], HttpResponse::HTTP_BAD_REQUEST);
@@ -75,16 +77,22 @@ class ProductController extends Controller
 
     public function destroy(DeleteProductRequest $request, int $id): \Illuminate\Http\JsonResponse
     {
-        if (!$request->user()->isAdministrator()) {
+        if (!$this->userIsAdministrator()) {
             return response()->json(["status" => EResponseStatus::ERROR, "message" => __("product.delete.forbidden")], HttpResponse::HTTP_FORBIDDEN);
         }
 
         try {
             $product = $this->products->byId($id);
             $this->products->deleteProduct($product);
+            $this->openSearchService->removeProduct($id);
             return response()->json(["status" => EResponseStatus::SUCCESS, "status_code" => HttpResponse::HTTP_OK, "message" => __("product.delete.success")], HttpResponse::HTTP_OK);
         } catch (ProductException $e) {
             return response()->json(["status" => EResponseStatus::FAILED, "message" => $e->getMessage()], HttpResponse::HTTP_BAD_REQUEST);
         }
+    }
+
+    private function userIsAdministrator(): bool
+    {
+        return request()->user()->isAdministrator();
     }
 }
