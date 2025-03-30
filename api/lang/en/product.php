9446f6a9cf15ce7193be5c4496e8d8cd45f7aaaf
Gabriel Santos <gabriel.code.org@gmail.com>
2025-03-30 00:19:14 -0300

    chore(lang): added new strings

--- api/lang/en/product.php
+++ api/lang/en/product.php
@@ -1,11 +1,27 @@
 <?php
 
 return [
+    'create' => [
+        'success' => 'Product has been created.',
+        'failed' => 'Occurred an error while trying to create product.',
+    ],
+    'delete' => [
+        'success' => 'Product has been deleted.',
+        'failed' => 'Occurred an error while trying to delete product.',
+    ],
+    'update' => [
+        'success' => 'Product has been updated.',
+        'failed' => 'Occurred an error while trying to update product.',
+    ],
     'category' => [
         'generic' => 'Generic',
         'furniture' => 'Furniture',
         'electronic' => 'Electronics',
         'health' => 'Health',
         'clothing' => 'Clothing',
-    ]
+    ],
+    'invalid_product' => 'Product does not exist or is invalid.',
+    'not_found' => 'Product not found.',
+    'invalid_data' => 'Invalid Product data.',
+    'category_not_found' => 'Product category could not be found.',
 ];
--- api/lang/pt_BR/product.php
+++ api/lang/pt_BR/product.php
@@ -1,11 +1,27 @@
 <?php
 
 return [
+    'create' => [
+        'success' => 'Produto foi cadastrado com sucesso.',
+        'failed' => 'Falha ao cadastrar produto.',
+    ],
+    'delete' => [
+        'success' => 'Produto foi removido com sucesso.',
+        'failed' => 'Falha ao remover produto.',
+    ],
+    'update' => [
+        'success' => 'Produto atualizado com sucesso.',
+        'failed' => 'Falha ao atualizar produto.',
+    ],
     'category' => [
         'generic' => 'Genéricos',
         'furniture' => 'Mobília',
         'electronic' => 'Eletrônicos',
         'health' => 'Saúde',
         'clothing' => 'Roupas',
-    ]
+    ],
+    'invalid_product' => 'Produto não existe ou é inválido.',
+    'not_found' => 'Produto não encontrado.',
+    'invalid_data' => 'Informações do produto são inválidas.',
+    'category_not_found' => 'A categoria do produto não foi encontrada.'
 ];
