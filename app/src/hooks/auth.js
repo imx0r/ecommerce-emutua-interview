699834a582e43f7ca41db90c7008f0c34d2ff77c
Gabriel Santos <gabriel.code.org@gmail.com>
2025-03-31 18:37:08 -0300

    chore(hook): use `swr` to fetch logged-in user data if there is a token and csrf request

--- app/src/hooks/auth.js
+++ app/src/hooks/auth.js
@@ -1,3 +1,23 @@
+import useSWR from 'swr'
+import axios from '@/lib/axios'
+
 export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
-    return {};
+    const { data: user, error, isLoading, mutate } = useSWR('/auth/me', async () => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            return axios
+                .get('/api/v1/auth/me', { headers: { 'Authorization': `Bearer ${token}` } })
+                .then(res => res.data)
+                .catch(error => {
+                    if (error.response.status !== 409) throw error
+                });
+        }
+    });
+
+    const csrf = () => axios.get('/sanctum/csrf-cookie')
+
+    return {
+        user,
+        isLoading
+    }
 }
