15e42f0da2b5add137eb97822559e384d838e5a1
Gabriel Santos <gabriel.code.org@gmail.com>
2025-03-31 18:39:49 -0300

    fix(hook): fixed `register` endpoint uri

--- app/src/hooks/auth.js
+++ app/src/hooks/auth.js
@@ -26,7 +26,7 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
         setErrors([])
 
         axios
-            .post('/register', props)
+            .post('/api/v1/auth/register', props)
             .then(() => mutate())
             .catch(error => {
                 if (error.response.status !== 422) throw error
