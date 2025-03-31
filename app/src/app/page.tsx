39efe3b728224418134a926d8343df2b3c6cdae6
Gabriel Santos <gabriel.code.org@gmail.com>
2025-03-31 19:26:28 -0300

    chore(ui): index (main) page

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,103 +1,122 @@
-import Image from "next/image";
+'use client';
+
+import useSWR from 'swr';
+import { useState } from 'react';
+import { useAuth } from '@/hooks/auth'
+import axios from "@/lib/axios";
+import Loading from "@/app/(app)/Loading";
+import Navigation from "@/app/Navigation";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    const { user, logout, isLoading } = useAuth();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    const { data: products, error, isLoading: isLoadingProducts, mutate: mutateProducts } = useSWR('/api/v1/products', async () => {
+        return axios.get('/api/v1/products')
+            .then(res => res.data)
+            .catch(error => {
+                console.error(error);
+            })
+    });
+    
+    const [search, setSearch] = useState('');
+    const [isSearching, setIsSearching] = useState(false);
+    
+    const searchProduct = async (e: any) => {
+        e.preventDefault();
+        
+        const result = await axios.get(`/api/v1/products/search?q=${search}`);
+        if (result.status === 200) {
+            setIsSearching(true);
+            
+            const data = result.data.hits.hits;
+            if (result.data.hits.total.value > 0) {
+                mutateProducts(data.map((e: any, i: number) => e._source), { revalidate: false });
+            } else {
+                mutateProducts([], { revalidate: false });
+            }
+        }
+    }
+    
+    const resetProducts = async () => {
+        setSearch('');
+        setIsSearching(false);
+        mutateProducts();
+    }
+    
+    if (isLoading) return (
+        <Loading />
+    );
+    
+    return (
+        <>
+            <div className="relative flex flex-col gap-2 max-w-5xl w-full min-h-screen mx-auto">
+                <Navigation />
+                <div className="flex flex-col w-full gap-2">
+                    <h2 className="text-2xl font-bold mt-3">Produtos</h2>
+                    <div className="flex flex-row items-baseline gap-2">
+                        <form onSubmit={searchProduct}>
+                            <div className="join my-3">
+                                <label className="floating-label">
+                                    <span>Buscar Produto</span>
+                                    <input type="text" placeholder="Buscar ..."
+                                           className="input input-bordered w-24 md:w-auto join-item"
+                                           value={search}
+                                           onChange={(e) => setSearch(e.target.value)}
+                                           required
+                                    />
+                                </label>
+                                <button type="submit" className="btn join-item">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none"
+                                         viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
+                                    </svg>
+                                </button>
+                            </div>
+                        </form>
+                        { (isSearching && !isLoadingProducts) && <button className="btn btn-neutral" onClick={resetProducts}>Ver todos os produtos</button> }
+                    </div>
+                    <div className="flex flex-row flex-wrap w-full gap-5">
+                        {isLoadingProducts ? (<span>Carregando produtos ...</span>) : products.length <= 0 ? (
+                            <>Nenhum produto foi encontrado!</>
+                        ) : 
+                        products.map((product: any, index: number) => {
+                            return (
+                                <div className="indicator" key={index}>
+                                    <span className="indicator-item indicator-top indicator-center badge badge-secondary">
+                                        {product.category}
+                                    </span>
+                                    <div className="card bg-base-200 w-82 shadow-sm">
+                                        <figure>
+                                            <img alt="produto"
+                                                 src="https://placehold.co/600x400/webp?text=Imagem+Produto"/>
+                                        </figure>
+                                        <div className="card-body">
+                                            <h2 className="card-title">{product.name}</h2>
+                                            <p>{product.description}</p>
+                                            <div className="flex flex-row justify-between items-baseline">
+                                                <span>R${product.price}</span>
+                                                <div className="card-actions justify-end">
+                                                    <button className="btn btn-success btn-sm">Comprar</button>
+                                                    <button className="btn btn-neutral btn-sm">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5"
+                                                             fill="none"
+                                                             viewBox="0 0 24 24" stroke="currentColor">
+                                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                                  strokeWidth="2"
+                                                                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"/>
+                                                        </svg>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
