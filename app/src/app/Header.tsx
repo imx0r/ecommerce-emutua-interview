3f3bebab6acad899ccb6edd461c1ebdc881ac0bd
Gabriel Santos <gabriel.code.org@gmail.com>
2025-03-31 22:22:57 -0300

    refactor(ui): added a actions prop to `Header` to be more dynamic

--- app/src/app/Header.tsx
+++ app/src/app/Header.tsx
@@ -1,10 +1,13 @@
-export default function Header({ title }: Readonly<{ title: string }>) {
+export default function Header({ title, actions }: Readonly<{ title: string, actions?: React.ReactNode }>) {
     return (
         <header className="bg-white">
             <div className="max-w-7xl mx-auto pt-6">
-                <h2 className="font-bold text-2xl text-gray-800 leading-tight">
-                    {title}
-                </h2>
+                <div className="flex flex-row justify-between">
+                    <h2 className="font-bold text-2xl text-gray-800 leading-tight">
+                        {title}
+                    </h2>
+                    {actions}
+                </div>
                 <div className="divider"></div>
             </div>
         </header>
